c2f2fa819f17fbd87f6678e8a277a3ec6a0fd340
George <msg993@gmail.com>
2020-03-11 13:35:53 +0300

    Changes to 4th step

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": {
+	"eslint:recommended",
+	"airbnb/base"
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": 0,
+        "import/extensions":0
+    }
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+<a href="https://codeclimate.com/github/Jorah-M/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/c65b211c19999419e657/maintainability" /></a>
